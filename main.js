d530009eaee190b054421ccf0040c7e12dd8ebc9
Nathan Cui <842168590@qq.com>
2019-03-29 17:22:15 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        activeName:'alert',
+        updateTime:'',
+        tableData: [],
+        search: ''
+    },
+    mounted: function() {
+        this.$http.get('./data.json', this.formquery,{emulateJSON:true}).then(function(response){
+          this.tableData = response.data.tableData;
+          this.updateTime = response.data.updateTime;
+        }, function(response){
+        // 响应错误回调
+        });
+    },
+    methods:{
+        onGoto(url) {
+            if(url){
+                window.open(url)
+            } else {
+                this.$message('本歌曲无连接，请至作品集中查看');
+            }
+        }
+    }
+});
