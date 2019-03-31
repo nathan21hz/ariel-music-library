b712157cd0fab22664e0969e9304d42d76c97e54
Nathan Cui <842168590@qq.com>
2019-03-31 20:30:55 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var app = new Vue({
         updateTime:'',
         tableData: [],
         search: '',
+        loading: true,
         form:{
             item:'',
             platform:'',
@@ -12,7 +13,7 @@ var app = new Vue({
         },
         rules:{
             item: [
-                { required: true, message: '请输入作品id', trigger: 'blur' }
+                { required: true, message: '请输入作品名', trigger: 'blur' }
             ],
             platform: [
                 { required: true, message: '请选择发布平台', trigger: 'blur' }
@@ -22,10 +23,43 @@ var app = new Vue({
             ]
         }
     },
+    computed: {
+        total: function () {
+            if(this.loading)
+                return 1
+            else
+                return this.tableData.length
+        },
+        netease_ratio:function(){
+            if(this.loading)
+                return 1
+            else
+                return (this.tableData.filter(data => data.netease != "").length / this.total * 100).toFixed(1)
+        },
+        sing_ratio:function(){
+            if(this.loading)
+                return 1
+            else
+                return (this.tableData.filter(data => data.sing != "").length / this.total * 100).toFixed(1)
+        },
+        qqmusic_ratio:function(){
+            if(this.loading)
+                return 1
+            else
+                return (this.tableData.filter(data => data.qqmusic != "").length / this.total * 100).toFixed(1)
+        },
+        other_ratio:function(){
+            if(this.loading)
+                return 1
+            else
+                return (this.tableData.filter(data => data.other != "").length / this.total * 100).toFixed(1)
+        }
+    },
     mounted: function() {
         this.$http.get('./data.json', this.formquery,{emulateJSON:true}).then(function(response){
           this.tableData = response.data.tableData;
           this.updateTime = response.data.updateTime;
+          this.loading = false
         }, function(response){
         // 响应错误回调
         });
@@ -49,14 +83,20 @@ var app = new Vue({
             this.onGoto(command)
         },
         onSubmit(){
-            this.$http.post('https://1509749986895836.cn-shanghai.fc.aliyuncs.com/2016-08-15/proxy/Automatic/neris-message/',this.form).then(function(response){
-                this.$message({
-                    message: response.data.message,
-                    type: response.data.type
-                });
-            }, function(response){
-                // 响应错误回调
-            });
+            this.$refs["form"].validate((valid) => {
+                if (valid) {
+                    this.$http.post('https://1509749986895836.cn-shanghai.fc.aliyuncs.com/2016-08-15/proxy/Automatic/neris-message/',this.form).then(function(response){
+                        this.$message({
+                            message: response.data.message,
+                            type: response.data.type
+                        });
+                    }, function(response){
+                        // 响应错误回调
+                    });
+                } else {
+                    return false
+                }
+            })
         }
     }
 });
